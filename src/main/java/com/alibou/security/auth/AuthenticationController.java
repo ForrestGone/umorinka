2964ffca89b23801a6192b2a103accf392105edd
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-06-29 16:02:12 +0000

    innitial commit

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -2,4 +2,5 @@ package com.alibou.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.P
+import org.springframework.web.bind.annotation.PostMapping;
+import org
