da37a6950b356af41f099336b5e18dd67b676cae
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-06-29 13:13:29 +0000

    merge from 1.0.

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -2,4 +2,4 @@ package com.alibou.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bin
+import org.springframework.web.bind.annotation.P
