30fa12c73c489481ad8f2d10991d1b018baac025
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-07-01 07:33:06 +0000

    remove deprecated method

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -3,4 +3,5 @@ package com.alibou.security.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotat
+import org.springframework.web.bind.annotation.RequestBody;
+impo
