aa89882bb50525c91ca6fbd287557b1e94d2c1fa
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-07-02 13:23:11 +0000

    Update Gemfile.lock accordingly

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -4,4 +4,4 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotat
+import org.springframework.web.bind.annotation.RequestMapping;
