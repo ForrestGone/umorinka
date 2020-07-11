6896c15b42d518baa494fe5f37843a8d2c3506d9
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-07-11 11:34:48 +0000

    Bugs solved

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -20,4 +20,5 @@ public class AuthenticationController {
   ) {
     return ResponseEntity.ok(service.register(request));
   }
-  @PostMapping("/authentica
+  @PostMapping("/authenticate")
+  public Respons
