21fdddb1ce39fac07cda6c94d8d91d5acb240f46
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-07-12 14:27:02 +0000

    0.2

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -21,4 +21,5 @@ public class AuthenticationController {
     return ResponseEntity.ok(service.register(request));
   }
   @PostMapping("/authenticate")
-  public ResponseEntity<AuthenticationResponse> authenticate
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @Reques
