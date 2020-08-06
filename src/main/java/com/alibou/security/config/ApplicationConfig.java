0f6d51b1d7440a120bd471fe0407e0592786a3ae
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-06 06:51:52 +0000

    Delete 4.png

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -2,4 +2,4 @@ package com.alibou.security.config;
 
 import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.co
+import org.springframework.context.annotation.Bean;
