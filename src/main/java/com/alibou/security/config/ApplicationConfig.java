951f9483ed736cd32e4f7af09d24d04d20996853
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-06 11:15:58 +0000

    Fix travis errors

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -3,4 +3,4 @@ package com.alibou.security.config;
 import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.sp
+import org.springframework.conte
