1609404b832fe4d605a9241464e2221be71d4e33
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-07 11:37:25 +0000

    add styling

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -3,4 +3,5 @@ package com.alibou.security.config;
 import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Co
+import org.springframework.context.annotation.Configuration;
+import
