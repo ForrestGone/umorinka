28999d594856dfcc171e9bec9ada07fd86206612
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-08 19:21:51 +0000

    Update DefaultLineJoining.java

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -4,4 +4,4 @@ import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authenticat
+import org.springframework.security.authentication.AuthenticationMa
