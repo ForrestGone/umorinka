06405cc03b9407665fb3ad380e96bdd4b315aed7
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-08 17:10:05 +0000

    next development cycle

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -4,4 +4,4 @@ import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.se
+import org.springframework.security.authenticat
