c87efe81ef251fbc79c4adf9b15b7d08fb3d0548
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-09 18:51:19 +0000

    add missing ;

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -4,4 +4,5 @@ import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationMa
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springf
