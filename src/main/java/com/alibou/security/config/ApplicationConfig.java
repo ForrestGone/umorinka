f6c4c3180d917289b564f7a41cca9a4b9ca7713c
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-11 11:20:34 +0000

    ASoC: wm8962: Update register CLASS_D_CONTROL_1 to be non-volatile

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -5,4 +5,5 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvi
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.spri
