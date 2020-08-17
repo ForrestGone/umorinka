72bb913abe6d042b62ee9aff3339046afbefa1dc
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-17 18:58:55 +0000

    update table

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -8,4 +8,5 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDeta
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.spri
