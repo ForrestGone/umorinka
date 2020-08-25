0022a85e228d19e987eb5aeb487cf8153d1bf816
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-25 06:24:05 +0000

    Run scan-local.sh on orientdb:...

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -15,4 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApplicationCon
+public class ApplicationConfig {
+
+  private f
