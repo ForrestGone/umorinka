94e973b03cbdb81ade858892bdf17af9fe5c7fff
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-08-29 21:22:43 +0000

    - include fix

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -22,4 +22,6 @@ public class ApplicationConfig {
   @Bean
   public UserDetailsService userDetailsService() {
     return username -> repository.findByEmail(username)
-        .orElseThrow(() -> new UsernameNotFoundException("User
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+
