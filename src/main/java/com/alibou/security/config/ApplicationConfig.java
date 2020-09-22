b08d4d84b08bbf86894709cee7ca26b490c84103
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2020-09-22 22:58:25 +0000

    Adding bumped version

--- src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -28,4 +28,4 @@ public class ApplicationConfig {
   @Bean
   public AuthenticationProvider authenticationProvider() {
     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDeta
+    authProvider.setUserDetailsService(userDetailsServi
