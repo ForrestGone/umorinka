257b0b86d666ff31bb67695ea68813f0f3b69720
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2021-06-20 15:30:06 +0000

    update sbt version

--- src/main/java/com/alibou/security/config/JwtService.java
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -16,4 +16,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-  private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B597
+  private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+  public St
