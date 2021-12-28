620723591bd65e0dd140e007753eafed7798a33d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2021-12-28 11:36:23 +0000

    remove theme

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -1,4 +1,5 @@
 package com.alibou.security.config;
 
 import com.alibou.security.token.TokenRepository;
-import jakarta.servlet.http.HttpServletR
+import jakarta.servlet.http.HttpServletRequest;
+import ja
