eae7d06dd3156c6a0b9d157460b18d4595e98243
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-01-02 08:20:25 +0000

    Better error message

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -2,4 +2,5 @@ package com.alibou.security.config;
 
 import com.alibou.security.token.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSe
+import jakarta.servlet.http.HttpServletResponse;
+im
