4f44e3b97da3d7cfdaced63a9c5b499303deb685
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-01-23 16:10:40 +0000

    Fixes #112

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -5,4 +5,5 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityConte
+import org.springframework.security.core.context.SecurityContextHolder;
+impo
