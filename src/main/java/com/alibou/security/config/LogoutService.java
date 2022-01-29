f32cb831c29dab4645edd455c01f0e8a43ed7379
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-01-29 21:08:06 +0000

    Added screenshots

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -6,4 +6,4 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.s
+import org.springframework.security.web.authentica
