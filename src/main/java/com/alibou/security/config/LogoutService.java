e801589799e36addbbb75a5a86b585b85c2122c4
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-02-13 10:50:13 +0000

    Bump copyright year

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -6,4 +6,4 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentica
+import org.springframework.security.web.authentication.logout.LogoutHandl
