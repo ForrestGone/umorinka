7ba3085782ba720ba80e9d3e185e6ed362253be8
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-02-18 08:21:57 +0000

    update travis file

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -6,4 +6,5 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.LogoutHandl
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springf
