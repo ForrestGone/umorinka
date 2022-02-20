9522da20e1ac6dd401941209c0f455fb54db602a
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-02-20 12:38:12 +0000

    from stefan

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -7,4 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.stereotype
+import org.springframework.stereotype.Service;
+
+@Service
