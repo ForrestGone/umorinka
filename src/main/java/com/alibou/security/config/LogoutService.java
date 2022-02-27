952f853c3660ea4c0396488422967664ad04c216
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-02-27 11:00:02 +0000

    updating .gitignore

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -13,4 +13,4 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
-  private final TokenReposito
+  private final TokenRepository tokenRepository;
