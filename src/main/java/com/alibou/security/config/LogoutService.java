ff14753116325191a620ea9b442d6608bed369b8
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-04-03 06:09:52 +0000

    Code style changes.

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -23,4 +23,4 @@ public class LogoutService implements LogoutHandler {
   ) {
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
-    if (authHeader == null 
+    if (authHeader == null ||!authHeader.startsWith
