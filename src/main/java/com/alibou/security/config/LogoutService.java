21202fd3508e79eef89c90a2d5c49c7a44dcf18c
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-04-03 07:11:44 +0000

    update revision number

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -23,4 +23,5 @@ public class LogoutService implements LogoutHandler {
   ) {
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
-    if (authHeader == null ||!authHeader.startsWith
+    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+      re
