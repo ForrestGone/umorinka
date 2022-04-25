9201b67b51276724fa03f05178739e1dccb82911
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-04-25 10:14:04 +0000

    servlet

--- src/main/java/com/alibou/security/config/LogoutService.java
+++ src/main/java/com/alibou/security/config/LogoutService.java
@@ -33,4 +33,6 @@ public class LogoutService implements LogoutHandler {
       storedToken.setExpired(true);
       storedToken.setRevoked(true);
       tokenRepository.save(storedToken);
-      SecurityContextHolder.clearCon
+      SecurityContextHolder.clearContext();
+    }
+ 
