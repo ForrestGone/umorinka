9083aec6e48d34b4c3c6ad0cf27af5558382948e
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-05-22 10:37:43 +0000

    compiled styles

--- src/main/java/com/alibou/security/demo/DemoController.java
+++ src/main/java/com/alibou/security/demo/DemoController.java
@@ -2,4 +2,4 @@ package com.alibou.security.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotatio
+import org.springframework.web.bind.annotation.RequestMapping;
