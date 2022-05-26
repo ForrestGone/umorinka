ef03e3da8d115da1f7c5c0ae6b6f825c734b153d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-05-26 12:25:35 +0000

    Add package info

--- src/main/java/com/alibou/security/demo/DemoController.java
+++ src/main/java/com/alibou/security/demo/DemoController.java
@@ -3,4 +3,4 @@ package com.alibou.security.demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframew
+import org.springframework.web.bind.a
