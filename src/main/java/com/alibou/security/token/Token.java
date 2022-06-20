24b7310d99a2826c9e8e3dff446cd4faa939b958
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-06-20 09:29:18 +0000

    f2fs: update start nid only once each circle

--- src/main/java/com/alibou/security/token/Token.java
+++ src/main/java/com/alibou/security/token/Token.java
@@ -3,4 +3,5 @@ package com.alibou.security.token;
 import com.alibou.security.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persisten
+import jakarta.persistence.EnumType;
+impor
