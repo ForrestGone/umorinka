cb52f33bf39a1df7a48132fa9a2429f5aeb7dfb7
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-07-04 22:16:03 +0000

    Add a missing include

--- src/main/java/com/alibou/security/token/Token.java
+++ src/main/java/com/alibou/security/token/Token.java
@@ -9,4 +9,5 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Builder;
