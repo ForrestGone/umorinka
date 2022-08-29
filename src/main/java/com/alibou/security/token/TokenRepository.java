c2d0ab07bd620f582fc04aed4a84cb545adfdfc6
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-08-29 19:19:27 +0000

    organize code

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -3,4 +3,6 @@ package com.alibou.security.token;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.
+import org.springframework.data.jpa.repository.Query;
+
+public in
