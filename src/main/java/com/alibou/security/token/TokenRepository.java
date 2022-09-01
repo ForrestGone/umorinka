6b1491a6782e7c6f02561d1337a6de23bcf9c969
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-09-01 12:53:04 +0000

    Sonar fixes.

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -5,4 +5,4 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public in
+public interface TokenRe
