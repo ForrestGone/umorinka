64425bc8d77b83c902490dd684bed317635d1ab6
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-09-04 15:33:50 +0000

    Add Default XML

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -5,4 +5,4 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface TokenRepository extends
+public interface TokenRepository extends JpaRepository<Token
