2b4ed4fa3021e65b2a2a88399769dc76a7286091
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-09-07 07:45:09 +0000

    serial: 8250_dw: Fix deadlock in LCR workaround

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -5,4 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface TokenRepository extends JpaRepository<Token
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+  @Quer
