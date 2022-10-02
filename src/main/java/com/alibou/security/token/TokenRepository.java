2db9857f36c5e90b82c25d15378d449f2425147d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-10-02 11:01:00 +0000

    pdate

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -9,4 +9,5 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
 
   @Query(value = """
       select t from Token t inner join User u\s
-      on t.user.id = u.id\
+      on t.user.id = u.id\s
+      where u.id = :id 
