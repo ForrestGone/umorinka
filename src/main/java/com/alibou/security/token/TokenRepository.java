ab1f74c89fbb0393a34baa5844528df58b86b222
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-10-02 20:36:31 +0000

    updated topo build

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -10,4 +10,4 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
   @Query(value = """
       select t from Token t inner join User u\s
       on t.user.id = u.id\s
-      where u.id = :id 
+      where u.id = :id and (t.expired = fa
