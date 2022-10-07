b285882324bd1aebcb909d8fc77fc7cd3655e50e
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-10-07 09:07:42 +0000

    r-programing

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -12,4 +12,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
       on t.user.id = u.id\s
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
-  List<Token> findAllValidTokenByUser(Intege
+  List<Token> findAllValidTokenByUser(Integer id);
+
+  Optional
