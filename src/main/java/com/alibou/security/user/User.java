9587e790fae4d5443129058a015859a07b73120d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-11-11 19:59:38 +0000

    cpufreq: add an helper to get/set cpu utilization to be used by mako_hotplug

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -3,4 +3,5 @@ package com.alibou.security.user;
 import com.alibou.security.token.Token;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
-import jakarta.persist
+import jakarta.persistence.Enumerated;
+i
