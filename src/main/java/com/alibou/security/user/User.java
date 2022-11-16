d1354371b7402b71c6191b717f5fc5e95b4a66f0
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-11-16 07:37:14 +0000

    add new stuff

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -4,4 +4,4 @@ import com.alibou.security.token.Token;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.p
+import jakarta.persistence.Generate
