8ebe7a09b04a2ca54aa53042588e71df35a5bb5d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-11-17 23:10:40 +0000

    structure changed

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -6,4 +6,4 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.p
+import jakarta.persistence.OneToMany;
