1b04ed874ffca2a0899d11ad47803605197d9499
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2022-11-17 14:39:05 +0000

    script cleanup

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -5,4 +5,5 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.
+import jakarta.persistence.Id;
+import jakarta.p
