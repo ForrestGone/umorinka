f1d079b0631eaee273b689f26f338f6e698190fb
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2023-01-08 10:20:01 +0000

    Release 1.5.4

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -42,4 +42,4 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new Simple
+    return List.of(new SimpleGrantedAuth
