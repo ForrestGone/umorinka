4ab20dc17323270f3dc78bebc931dfa2759e684d
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2023-02-11 22:24:27 +0000

    klist: fix starting point removed bug in klist iterators

--- src/main/java/com/alibou/security/user/UserRepository.java
+++ src/main/java/com/alibou/security/user/UserRepository.java
@@ -3,4 +3,6 @@ package com.alibou.security.user;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<U
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  Optio
