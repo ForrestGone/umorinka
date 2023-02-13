6c62af18d876d6df1a11ba3b1fe24569a2cbfcf4
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2023-02-13 12:56:12 +0000

    New translations strings.xml (Bulgarian)

--- src/main/java/com/alibou/security/user/UserRepository.java
+++ src/main/java/com/alibou/security/user/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-  Optional<User> findByEmail(String
+  Optional<User> findByEmail(String email);
+
+}
