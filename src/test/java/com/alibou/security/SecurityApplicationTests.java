f3ea7a783d3546d6ff9c19b475736f2eb18ef1bf
ForrestGone <108995335+ForrestGone@users.noreply.github.com>
2023-03-03 18:41:37 +0000

    fixed #3

---
+++ src/test/java/com/alibou/security/SecurityApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.alibou.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
